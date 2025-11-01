14da9ab5659d2a3e9fb9e455cc92e429df42a716
AmayaKuro <vhtkiet9615@gmail.com>
2025-11-01 10:58:07 +0700

    fuk up big time

--- EXE_BE/Controllers/ViewModel/LoginRequest.cs
+++ EXE_BE/Controllers/ViewModel/LoginRequest.cs
@@ -5,8 +5,7 @@ namespace EXE_BE.Controllers.ViewModel
     public class LoginRequest
     {
         [Required]
-        [EmailAddress]
-        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
 
         [Required]
         public string Password { get; set; } = string.Empty;
